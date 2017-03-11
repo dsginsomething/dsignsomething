bbba4a3cb0ff2e1a100cb646f3dd16c9745be745
Smart Worapon <Smart Worapon>
2017-03-11 20:58:08 +0700

    add custiom search dview, dsigner(no filter)

---
+++ src/themes/dsignsomething/custom-search/dsigner-search.php
@@ -0,0 +1,12 @@
+<div class="row">
+ <div class="sixteen wide column">
+  <form role="search" action="<?php echo site_url('/'); ?>" method="get" id="searchform" class="#searchform ui form">
+   <div class="ui fluid action input">
+     <input name="tags" type="text" placeholder="Search..." />
+     <button type="submit" alt="Search" value="Search" class="ui icon basic button">
+       <i class="search icon"></i>
+     </button>
+   </div>
+  </form>
+ </div>
+</div>
---
+++ src/themes/dsignsomething/custom-search/dwell-search.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <form role="search" action="<?php echo site_url('/'); ?>" method="post" id="searchform" class="#searchform ui form">
+    <div class="fields">
+      <div class="two wide field">
+        <button type="submit" name="submit" value="all" class="ui button basic fluid">ALL</button>
+      </div>
+      <div class="six wide field">
+        <button type="submit" name="submit" value="thai" class="ui button basic fluid">THAI</button>
+      </div>
+      <div class="eight wide field">
+        <button type="submit" name="submit" value="international" class="ui button basic fluid">INTERNATIONAL</button>
+      </div>
+    </div>
+  </form>
+</div>

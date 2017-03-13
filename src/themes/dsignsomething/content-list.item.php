035ad62e8e8b5eb646f11d520b31dffc71f64d80
ChinCluBi <chinnaporn.s@ku.th>
2017-03-13 22:45:37 +0700

    added dtalk custom page

--- src/themes/dsignsomething/content-list.item.php
+++ src/themes/dsignsomething/content-list.item.php
@@ -1,10 +1,13 @@
-<div class="card">
+<?php
+if( $dsignsomething_is_single_right ) :
+?>
+<div class="card right">
 	<div class="image">
 		<?php
 		if( has_post_thumbnail() ) :
-			dsignsomething_image(get_the_post_thumbnail_url(null), 300);
+			dsignsomething_image(get_the_post_thumbnail_url(null), "200px");
 		else :
-			dsignsomething_image(get_template_directory_uri()."/images/thumbnail-white.png", 300);
+			dsignsomething_image(get_template_directory_uri()."/images/thumbnail-white.png", "200px");
 		endif;
 		?>
 	</div>
@@ -24,5 +27,39 @@
 			if( $dsignsomething_is_single ) : the_content("<div></div>"); endif;
 		?>
 	</div>
-	<div class="ui divider"></div>
 </div>
+<div class="ui divider"></div>
+<?php
+else :
+?>
+<div class="card">
+	<div class="image">
+		<?php
+		if( has_post_thumbnail() ) :
+			dsignsomething_image(get_the_post_thumbnail_url(null), $dsignsomething_image_height ?: "300px");
+		else :
+			dsignsomething_image(get_template_directory_uri()."/images/thumbnail-white.png", $dsignsomething_image_height ?: "300px");
+		endif;
+		?>
+	</div>
+	<div class="content">
+		<div class="meta">
+			<div class="ui horizontal divided list">
+				<div class="item">
+					<?php echo strtoupper(get_post_time('M Y, d', true)); ?>
+				</div>
+				<div class="item">
+					<?php echo get_the_category()[0]->name; ?>
+				</div>
+			</div>
+		</div>
+		<?php 
+			the_title( sprintf( '<a class="header" href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' );
+			if( $dsignsomething_is_single ) : the_content("<div></div>"); endif;
+		?>
+	</div>
+</div>
+<div class="ui divider"></div>
+<?php 
+endif;
+?>
